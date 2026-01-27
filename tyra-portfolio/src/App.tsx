5a0309621c1c3a3fee2be1b649e265f6fe7b72a7
alexeimowat <56664562+alexeimowat@users.noreply.github.com>
2026-01-26 19:13:58 -0700

    first commit

---
+++ tyra-portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-zinc-900 text-zinc-100">
+      <h1 className="text-4xl font-bold tracking-tight">hi :)</h1>
+    </div>
+  )
+}
+
+export default App
