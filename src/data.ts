1d6d13aec0326477c373fb01dcaa932ffa7ed538
alexeimowat <56664562+alexeimowat@users.noreply.github.com>
2026-01-26 21:54:30 -0700

    template for porfolio projects

---
+++ src/data.ts
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+    id: 1,
+    title: "Environmental Technology Field School",
+    category: "Field Work",
+    location: "Kananaskis, AB",
+    description: "During an intensive, on-site field school with SAIT, I lived in a remote camp environment while conducting daily environmental sampling and site assessments. I was responsible for collecting diverse datasets on river health, plant species, and wildlife populations under demanding conditions. Throughout the program, I maintained high performance over extended hours while strictly applying field safefy protocols in a remote setting.",
+    image: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop" 
+  },
+  {
+    id: 2,
+    title: "Riparian Restoration Volunteer",
+    category: "Volunteer",
+    location: "Calgary, AB",
+    description: "As a Riparian Restoration Volunteer, I collaborated with organizations like Cows and Fish, AWES, and the Ghost Watershed Alliance Society to improve local ecosystem stability. I actively assisted in restoration efforts by planting willow states, a key technique used to provide natural erosion control along streambanks.",
+    image: "https://images.unsplash.com/photo-1500382017468-9049fed747ef?q=80&w=2832&auto=format&fit=crop"
+  },
+  {
+    id: 3,
+    title: "Water Quality Monitoring Volunteer",
+    category: "Volunteer",
+    location: "Calgary, AB",
+    description: "As a Water Quality Monitoring volunteer with the CreekWatch program, I conducted weekly assessments of a local stream to evaluate its overall health. Working as a team, I gathered precise data on pH, dissolved oxygen (DO), turbidity, and nutrient levels, while also performing invertebrate counts. These efforts provided essential data for monitoring long-term environmental trends and water quality standards.",
+    image: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=2670&auto=format&fit=crop"
+  }
+]
