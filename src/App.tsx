de9dcffcac114327a828fc46e205232c6cdeb016
alexeimowat <56664562+alexeimowat@users.noreply.github.com>
2026-01-26 21:13:07 -0700

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-zinc-900 text-zinc-100">
+    <div className="flex h-screen w-full items-center justify-center bg-zinc-900 text-pink-500">
       <h1 className="text-4xl font-bold tracking-tight">hi :)</h1>
     </div>
   )
