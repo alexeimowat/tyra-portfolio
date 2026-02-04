32e3d771eb50e67598f9bcda739ae9119fbf827a
alexeimowat <56664562+alexeimowat@users.noreply.github.com>
2026-02-03 19:07:57 -0700

    update descriptions, add a new volunteer role

--- src/data.ts
+++ src/data.ts
@@ -4,23 +4,31 @@ export const projects = [
     title: "Environmental Technology Field School",
     category: "Field Work",
     location: "Kananaskis, AB",
-    description: "During an intensive, on-site field school with SAIT, I lived in a remote camp environment while conducting daily environmental sampling and site assessments. I was responsible for collecting diverse datasets on river health, plant species, and wildlife populations under demanding conditions. Throughout the program, I maintained high performance over extended hours while strictly applying field safefy protocols in a remote setting.",
+    description: "During an intensive on-site program with SAIT, I lived in a remote camp while conducting an environmental assessment. I spent long hours in the beautiful area of the Kananaskis mountains, where I collected a variety of environmental samples. I analyzed river and soil health using multiparameter meters, in-situ sondes and invertebrate counts. I also assessed levels of heavy metals in the forest through an extensive experiment on lichen populations. Lastly, I observed wildlife biodiversity through deployment of trail cameras and eDNA studies.",
     image: "/images/field-school.webp" 
   },
   {
     id: 2,
-    title: "Riparian Restoration Volunteer",
+    title: "Riverbank Reclamation",
     category: "Volunteer",
-    location: "Calgary, AB",
-    description: "As a Riparian Restoration Volunteer, I collaborated with organizations like Cows and Fish, AWES, and the Ghost Watershed Alliance Society to improve local ecosystem stability. I actively assisted in restoration efforts by planting willow states, a key technique used to provide natural erosion control along streambanks.",
-    image: "/images/riparian.webp"
+    location: "South Ghost Provincial Recreation Area, AB",
+    description: "In the remote locations of the Ghost Watershed, many riverbanks are unstable and eroding because of off-road vehicles. I helped restore disturbed areas through removing debris, building natural drain trenches, planting native trees and willow staking.",
+    image: "/images/riparian-volunteer.webp"
   },
   {
     id: 3,
+    title: "Riparian Restoration",
+    category: "Volunteer",
+    location: "Didsbury, AB",
+    description: "In collaboration with organizations such as Cows and Fish, AWES and Mountain View County, I assisted with restoring the riparian zone at a local farm. Cattle farmers face destruction to their home as cows will trample areas around a stream. By working alongside the farmers, cows were deterred and the riparian zone was restored through wattle fences and willow staking.",
+    image: "/images/didsbury.webp"
+  },
+  {
+    id: 4,
     title: "Water Quality Monitoring Volunteer",
     category: "Volunteer",
     location: "Calgary, AB",
-    description: "As a Water Quality Monitoring volunteer with the CreekWatch program, I conducted weekly assessments of a local stream to evaluate its overall health. Working as a team, I gathered precise data on pH, dissolved oxygen (DO), turbidity, and nutrient levels, while also performing invertebrate counts. These efforts provided essential data for monitoring long-term environmental trends and water quality standards.",
+    description: "As a CreekWatch volunteer, I monitor my local stream for water quality. I conduct weekly assessments with my team by gathering precise data on pH, dissolved oxygen, turbidity, nutrient levels and more. I also collect and analyze invertebrate species, especially Mayflies, an important indicator species. These efforts provide essential information for long-term environmental trends.",
     image: "/images/creekWatch.webp"
   }
 ]
